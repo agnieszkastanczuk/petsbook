7288f0d635f1c65fdba0cbb4fdd5f9e1ab5eb956
Agnieszka S-D <work.a.stanczuk@gmail.com>
2023-07-13 11:47:26 +0200

    add UserSchema and register

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+import { register } from "./controllers/auth.js";
 
 
 /* FILE STORAGE */
@@ -34,6 +35,10 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
