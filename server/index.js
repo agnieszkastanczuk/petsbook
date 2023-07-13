5e922c9f0593b7244b6456fb85cdd8653a4972a1
Agnieszka S-D <work.a.stanczuk@gmail.com>
2023-07-13 12:14:39 +0200

    add logging in

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import authRoutes from "./routes/auth.js"
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -22,8 +24,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
-import { register } from "./controllers/auth.js";
-
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -39,6 +39,8 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
