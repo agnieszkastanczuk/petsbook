fbab3a1a4ec767285579ccd1f7d803294182df44
Agnieszka S-D <work.a.stanczuk@gmail.com>
2023-07-13 14:38:49 +0200

    add post routers and fix bugs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { login } from '../controllers/auth';
+import express from "express";
+import { login } from "../controllers/auth.js";
 
 const router = express.Router();
 
