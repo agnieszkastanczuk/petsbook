dc101e46219fb17fd70db11539a1313a3e7e62e2
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-07-23 12:55:21 +0200

    add PostsWidget and PostWidget

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -44,7 +44,7 @@ export const addRemoveFriend = async (req, res) => {
             friend.friends = friend.friends.filter((id) => id !== id);
         } else {
             user.friends.push(friendId);
-            friends.friend.push(id);
+            friend.friends.push(id);
         }
         await user.save();
         await friend.save();
